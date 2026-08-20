255a2915bff373e9f41d1e499e33e25f3074bc5b
= <4128973+DasKlaus@users.noreply.github.com>
2026-08-20 07:48:39 +0200

    log, don't display errors

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
 date_default_timezone_set('Europe/Berlin');
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
